a8d35f0b5108c216ec588dd03ad69149f8888d41
Will Kusper <wkusper@iu.edu>
2024-01-28 19:06:07 -0500

    initial commit

---
+++ src/test/java/edu/iu/wkusper/guitar/controllers/GuitarControllerTest.java
@@ -0,0 +1,46 @@
+package edu.iu.wkusper.guitar.controllers;
+
+import org.junit.jupiter.api.Test;
+import edu.iu.wkusper.guitar.model.Guitar;
+import static org.junit.jupiter.api.Assertions.*;
+
+class GuitarControllerTest {
+
+    @Test
+    void add() {
+    GuitarController guitarController = new GuitarController();
+    assertTrue(guitarController.add(new Guitar("123", "Fender", "Stratocaster", "electric", "maple", "alder", 1499.99)));
+    assertTrue(guitarController.add(new Guitar("456", "Gibson", "Les Paul", "electric", "mahogany", "mahogany", 2499.99)));
+    }
+
+    boolean sameGuitar(Guitar guitar1, Guitar guitar2) {
+        return guitar1.getSerialNumber().equals(guitar2.getSerialNumber()) &&
+                guitar1.getBuilder().equals(guitar2.getBuilder()) &&
+                guitar1.getModel().equals(guitar2.getModel()) &&
+                guitar1.getType().equals(guitar2.getType()) &&
+                guitar1.getBackWood().equals(guitar2.getBackWood()) &&
+                guitar1.getTopWood().equals(guitar2.getTopWood()) &&
+                guitar1.getPrice() == guitar2.getPrice();
+    }
+    @Test
+    void find() {
+        GuitarController guitarController = new GuitarController();
+        assertTrue(sameGuitar(new Guitar("123", "Fender", "Stratocaster", "electric", "maple", "alder", 1499.99), guitarController.find("123")));
+        assertTrue(sameGuitar(new Guitar("456", "Gibson", "Les Paul", "electric", "mahogany", "mahogany", 2499.99), guitarController.find("456")));
+    }
+
+    @Test
+    void search() {
+        GuitarController guitarController = new GuitarController();
+        assertEquals(4, guitarController.search("", "", "", "", "", "", 0).size());
+        assertEquals(1, guitarController.search("123", "", "", "", "", "", 0).size());
+        assertEquals(1, guitarController.search("", "Fender", "", "", "", "", 0).size());
+        assertEquals(1, guitarController.search("", "", "Stratocaster", "", "", "", 0).size());
+        assertEquals(2, guitarController.search("", "", "", "electric", "", "", 0).size());
+        assertEquals(1, guitarController.search("", "", "", "", "mahogany", "", 0).size());
+        assertEquals(1, guitarController.search("", "", "", "", "", "alder", 0).size());
+        assertEquals(1, guitarController.search("", "Gibson", "", "", "", "", 0).size());
+        assertEquals(1, guitarController.search("", "", "", "electric", "mahogany", "", 0).size());
+        assertEquals(0, guitarController.search("", "", "", "bass", "", "", 0).size());
+    }
+}
